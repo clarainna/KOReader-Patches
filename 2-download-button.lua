93f628cace75afd27b10be4925ee5824c73e64d2
clarainna <126372702+clarainna@users.noreply.github.com>
2025-11-12 10:23:38 +0900

    the patch now actually works on kobo !
    Added a new download tab to the file manager menu with specific items.

--- 2-download-button.lua
+++ 2-download-button.lua
@@ -1,68 +1,89 @@
--- Custom menu tabs (Only make changes here to add tabs) -----------------------------------------------
-local subMenu = {
-    -- tab_name = { icon = "tab_icon_name"},
-    download = { icon = "appbar.download"},
-}
-------------------------------------------------------------------
+-- This runs immediately when patch loads (safe)
+local function createDownloadTab()
+    -- This code runs later, after KOReader is initialized
+    local DataStorage = require("datastorage")
+    local FileManagerMenu = require("apps/filemanager/filemanagermenu")
+    local fileMangerMenuInitOriginal = FileManagerMenu.init
 
-local FileManagerMenu = require("apps/filemanager/filemanagermenu")
-local fileMangerMenuInitOriginal = FileManagerMenu.init
+    -- Custom menu tabs (Only make changes here to add tabs) -----------------------------------------------
+    local subMenu = {
+        -- tab_name = { icon = "tab_icon_name"},
+        download = { icon = "appbar.download"},
+    }
+    ------------------------------------------------------------------
+    local DataStorage = require("datastorage")
+    local logger = require("logger")
 
-function FileManagerMenu:init()
-    fileMangerMenuInitOriginal(self)
+    local FileManagerMenu = require("apps/filemanager/filemanagermenu")
+    local fileMangerMenuInitOriginal = FileManagerMenu.init
 
-    for k, v in pairs(subMenu) do
-        self.menu_items[k] =  v
+    function FileManagerMenu:init()
+        fileMangerMenuInitOriginal(self)
+
+        for k, v in pairs(subMenu) do
+            self.menu_items[k] =  v
+        end
+    end
+
+    code = [[
+        return {
+            ["KOMenu:menu_buttons"] = {
+                "filemanager_settings",
+                "setting",
+                "tools",
+                "search",
+                "download", -- new download tab
+                "plus_menu",
+                "main",
+            },
+            
+        -- Remove menu items you want to add to new tab from old tabs while keeping the menu items you want to keep in the same location (--- comments out the line, removes the item)
+        --- tools = {
+                --- ...
+                --- "cloud_storage",
+                ---"move_to_archive",
+                --- "wallabag",
+                --- ...
+            ---},
+            search = {
+                --- ...
+                ---"zlibrary_main",
+                ---"rakuyomi",
+            },
+            
+            -- Add removed menu items to the new tabs
+            download = { -- new download tab menu items
+                "zlibrary_main",
+                "rakuyomi",
+            },
+        }
+    ]]
+
+    local filepath = DataStorage:getSettingsDir() .. "/filemanager_menu_order.lua"
+
+    -- Try to read existing file
+    local file = io.open(filepath, "r")
+    local existing_content = ""
+    if file then
+        existing_content = file:read("*all")
+        file:close()
+    end
+
+    -- Only write if content is different
+    if existing_content ~= code then
+        file = io.open(filepath, "w")
+        if file then
+            file:write(code)
+            file:close()
+            logger.info("Menu order file updated")
+        else
+            logger.err("Failed to write menu order file: " .. filepath)
+        end
+    else
+        logger.info("Menu order file already up to date")
     end
 end
 
-code = [[
-    return {
-        ["KOMenu:menu_buttons"] = {
-            "filemanager_settings",
-            "setting",
-            "tools",
-            "search",
-            "download", -- new download tab
-            "plus_menu",
-            "main",
-        },
-        
-    -- Remove menu items you want to add to new tab from old tabs while keeping the menu items you want to keep in the same location (--- comments out the line, removes the item)
-    --- tools = {
-            --- ...
-            --- "cloud_storage",
-            ---"move_to_archive",
-            --- "wallabag",
-            --- ...
-        ---},
-        search = {
-            --- ...
-            ---"zlibrary_main",
-            ---"rakuyomi",
-        },
-        
-        -- Add removed menu items to the new tabs
-        download = { -- new download tab menu items
-            "zlibrary_main",
-            "rakuyomi",
-        },
-    }
-]]
-
-local filepath = "koreader/settings/filemanager_menu_order.lua"
-
--- Try to read existing file
-local file = io.open(filepath, "r")
-local existing_content = ""
-if file then
-    existing_content = file:read("*all")
-    file:close()
-end
-
--- Only write if content is different
-if existing_content ~= code then
-    file = io.open(filepath, "w")
-    file:write(code)
-    file:close()
-end
+-- Schedule to run after initialization
+local UIManager = require("ui/uimanager")
+UIManager:scheduleIn(0.1, createDownloadTab)
